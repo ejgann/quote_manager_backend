52d6451193faa8dde1a7b6d9d57044fe19ecb0bd
Emily Gann <aspentree79@gmail.com>
2020-12-09 07:15:34 -0500

    Initial commit

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::ProjectsController < ApplicationController
+
+    def index
+        projects = Project.all
+        render json: projects
+
+    end
+
+    def show
+
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:name, :budget)
+
+    end
+
+
+end
