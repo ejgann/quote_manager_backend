95457dcc3db13573ae959a3f8d28d2ac98b53a34
Emily Gann <aspentree79@gmail.com>
2021-01-04 17:30:51 -0500

    Updated controller and route files to accurately reflect the actually HTTP actions

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -5,10 +5,6 @@ class Api::V1::ProjectsController < ApplicationController
         render json: ProjectSerializer.new(projects)
     end
 
-    def show
-        project = Project.find_by(id: params[:id])
-        render json: ProjectSerializer.new(project)
-    end
 
     private
 
